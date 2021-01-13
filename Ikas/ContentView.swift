57ad404aa304259d083c25cc8954aa51b403807e
Xie Zhihao <xzh1206@gmail.com>
2021-01-14 01:26:48 +0800

    Initial Commit

---
+++ Ikas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ikas
+//
+//  Created by Sketch on 2021/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ikas/IkasApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkasApp.swift
+//  Ikas
+//
+//  Created by Sketch on 2021/1/14.
+//
+
+import SwiftUI
+
+@main
+struct IkasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
