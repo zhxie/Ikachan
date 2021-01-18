2c802aa732cd4b2485b5519a2c994320c999d047
Xie Zhihao <xzh1206@gmail.com>
2021-01-18 14:10:54 +0800

    Fix preview in content view

--- Ikachan/Views/ContentView.swift
+++ Ikachan/Views/ContentView.swift
@@ -46,5 +46,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
