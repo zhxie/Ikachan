636a35537b9a997f8f3241835a113b5c950f46c0
Xie Zhihao <xzh1206@gmail.com>
2024-02-28 14:02:12 +0800

    Invert unknown weapon icon in light background

--- Ikachan/Views/Components/WeaponsView.swift
+++ Ikachan/Views/Components/WeaponsView.swift
@@ -2,21 +2,39 @@ import SwiftUI
 import Kingfisher
 
 struct WeaponsView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var weapons: [Weapon]
     
     var body: some View {
         HStack(spacing: 4) {
             ForEach(weapons, id: \.name) { weapon in
-                Color.clear
-                    .aspectRatio(1, contentMode: .fit)
-                    .overlay(
-                        KFImage(weapon.image)
-                            .fade(duration: 0.5)
-                            .resizedToFit(1)
-                            .accessibilityLabel(weapon.name)
-                    )
+                if shouldInvert(weapon: weapon) {
+                    Color.clear
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay(
+                            KFImage(weapon.image)
+                                .fade(duration: 0.5)
+                                .resizedToFit(1)
+                                .accessibilityLabel(weapon.name)
+                                .colorInvert()
+                        )
+                } else {
+                    Color.clear
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay(
+                            KFImage(weapon.image)
+                                .fade(duration: 0.5)
+                                .resizedToFit(1)
+                                .accessibilityLabel(weapon.name)
+                        )
+                }
             }
         }
+    }
+    
+    func shouldInvert(weapon: Weapon) -> Bool {
+        colorScheme == .light && weapon.image.absoluteString.contains("a23d035e2f37c502e85b6065ba777d93f42d6ca7017ed029baac6db512e3e17f")
     }
 }
 
--- IkachanWidget/Views/WeaponsView.swift
+++ IkachanWidget/Views/WeaponsView.swift
@@ -2,21 +2,40 @@ import SwiftUI
 import Kingfisher
 
 struct WeaponsView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var weapons: [Weapon]
     
     var body: some View {
         HStack(spacing: 4) {
             ForEach(weapons, id: \.name) { weapon in
-                Color.clear
-                    .aspectRatio(1, contentMode: .fit)
-                    .overlay(
-                        KFImage(weapon.thumbnail ?? weapon.image)
-                            .resizedToFill()
-                            .clipped()
-                            .accessibilityLabel(weapon.name)
-                    )
-                    .cornerRadius(8)
+                if shouldInvert(weapon: weapon) {
+                    Color.clear
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay(
+                            KFImage(weapon.thumbnail ?? weapon.image)
+                                .resizedToFill()
+                                .clipped()
+                                .accessibilityLabel(weapon.name)
+                                .colorInvert()
+                        )
+                        .cornerRadius(8)
+                } else {
+                    Color.clear
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay(
+                            KFImage(weapon.thumbnail ?? weapon.image)
+                                .resizedToFill()
+                                .clipped()
+                                .accessibilityLabel(weapon.name)
+                        )
+                        .cornerRadius(8)
+                }
             }
         }
+    }
+    
+    func shouldInvert(weapon: Weapon) -> Bool {
+        colorScheme == .light && weapon.image.absoluteString.contains("a23d035e2f37c502e85b6065ba777d93f42d6ca7017ed029baac6db512e3e17f")
     }
 }
