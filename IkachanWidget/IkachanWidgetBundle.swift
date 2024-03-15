384767a4d633f8225783d135dffd19fb071e1a30
Xie Zhihao <xzh1206@gmail.com>
2024-03-15 13:51:16 +0800

    Disable interactive widgets for compatbility Widgets may disappear in iOS 16 with interactive widgets for iOS 17

--- IkachanWidget/IkachanWidgetBundle.swift
+++ IkachanWidget/IkachanWidgetBundle.swift
@@ -4,27 +4,19 @@ import SwiftUI
 struct IkachanWidgetBundle: WidgetBundle {
     @WidgetBundleBuilder
     var body: some Widget {
+        Splatoon3ScheduleWidget()
+        Splatoon3ShiftWidget()
+        Splatoon2ScheduleWidget()
+        Splatoon2ShiftWidget()
         if #available(iOSApplicationExtension 16.0, *) {
             Splatoon3ScheduleDynamicWidget()
-        }
-        Splatoon3ScheduleWidget()
-        if #available(iOSApplicationExtension 17.0, *) {
-            Splatoon3ScheduleInteractiveWidget()
-        }
-        if #available(iOSApplicationExtension 16.0, *) {
             Splatoon3ShiftDynamicWidget()
-        }
-        Splatoon3ShiftWidget()
-        if #available(iOSApplicationExtension 16.0, *) {
             Splatoon2ScheduleDynamicWidget()
-        }
-        Splatoon2ScheduleWidget()
-        if #available(iOSApplicationExtension 17.0, *) {
-            Splatoon2ScheduleInteractiveWidget()
-        }
-        if #available(iOSApplicationExtension 16.0, *) {
             Splatoon2ShiftDynamicWidget()
         }
-        Splatoon2ShiftWidget()
+//        if #available(iOSApplicationExtension 17.0, *) {
+//            Splatoon3ScheduleInteractiveWidget()
+//            Splatoon2ScheduleInteractiveWidget()
+//        }
     }
 }
