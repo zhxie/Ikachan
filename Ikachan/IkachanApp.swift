a33322f8cf20227af12b1a3459a473adc9b4ae5e
Xie Zhihao <xzh1206@gmail.com>
2024-11-29 15:06:53 +0800

    Terminate the app on macOS if the window is closed

--- Ikachan/IkachanApp.swift
+++ Ikachan/IkachanApp.swift
@@ -3,6 +3,8 @@ import Kingfisher
 
 @main
 struct IkachanApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     init() {
         KingfisherManager.shared.downloader.downloadTimeout = Timeout
         KingfisherManager.shared.cache.diskStorage.config.expiration = .never
@@ -11,6 +13,22 @@ struct IkachanApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func sceneDidDisconnect(_ scene: UIScene) {
+        if ProcessInfo.processInfo.isiOSAppOnMac {
+            exit(0)
         }
     }
 }
